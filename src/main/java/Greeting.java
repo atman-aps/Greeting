64a2d3b9d42d61cefa2f5d81b83b6dd951fea718
atman <atman.aps@gmail.com>
2020-08-07 01:22:17 +0700

    добавдение коммитов

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -2,7 +2,7 @@ public class Greeting {
 
     public static void main (String[]args) {
 
-        System.out.println("Привет! Я учу Java!");
+        System.out.println("Привет! Я учу Java! Учусь добавлять коммиты");
 
     }
 }
